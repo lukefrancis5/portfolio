b3c0d808a987617668096df449c87c17f497af61
lukefrancis5 <lukebrandonfrancis@gmail.com>
2019-01-17 14:31:43 -0500

    modified blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
+from . import views  
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home'),
-    path('blog/', include('blog.urls')),
+    path('', views.allblogs, name='allblogs'),
 ] 
